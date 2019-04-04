ca972a8ff0717a00ba66177a02fbc63a88de382c
Marvin Löbel <loebel.marvin@gmail.com>
2019-04-04 23:53:16 +0200

    add keyhook option

--- .scripts/wsl/windows/start_xserver.ps1
+++ .scripts/wsl/windows/start_xserver.ps1
@@ -1,4 +1,4 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Screen]::AllScreens | out-file -encoding ASCII C:\wsl\screen_geometry.txt
-& "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -nodecoration -wgl -clipboard -multimonitors
+& "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -nodecoration -wgl -clipboard -multimonitors -keyhook
 & ubuntu1804 -c "~/.scripts/wsl/wlaunch /mnt/c/wsl/screen_geometry.txt"
