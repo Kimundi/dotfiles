ae960e29bf0da1f2e0f7b85f6511770f77b4665d
Marvin Löbel <loebel.marvin@gmail.com>
2019-04-04 12:06:16 +0200

    Don't install dotfiles to bashrc directly

--- .scripts/system_setup/install_dotfiles_commands.sh
+++ .scripts/system_setup/install_dotfiles_commands.sh
@@ -4,5 +4,6 @@ shopt -s expand_aliases
 git init --bare $HOME/.dotfiles.git
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'" >> $HOME/.bashrc
+echo "Add the following alias to your shell:"
+echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'"
 
