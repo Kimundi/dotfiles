0be32f5a3e0edd4d523c243ebcffc6e5f88c5d6e
Marvin Löbel <loebel.marvin@gmail.com>
2025-06-14 18:00:48 +0200

    Set local branch to master always

--- .dotfiles/system_setup/install_dotfiles_commands.sh
+++ .dotfiles/system_setup/install_dotfiles_commands.sh
@@ -4,7 +4,7 @@ set -e
 
 function setup {
     mkdir -p $HOME/.dotfiles
-    git init --bare $HOME/.dotfiles/git_dir
+    git init --bare $HOME/.dotfiles/git_dir -b master
     dotfiles="git --git-dir=$HOME/.dotfiles/git_dir --work-tree=$HOME"
     $dotfiles remote add origin git@github.com:Kimundi/dotfiles.git
     $dotfiles fetch
