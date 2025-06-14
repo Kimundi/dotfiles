efb68d13948fabcefef07775ec80b32699795627
Marvin Löbel <loebel.marvin@gmail.com>
2025-06-14 18:13:45 +0200

    fix fishlogin install script

--- .dotfiles/system_setup/install_fish_bash_inheriting_env.sh
+++ .dotfiles/system_setup/install_fish_bash_inheriting_env.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sudo cp ~/.scripts/system_setup/fishlogin /usr/local/bin/fishlogin
+set -e
+
+sudo cp ~/.dotfiles/system_setup/fishlogin /usr/local/bin/fishlogin
 echo /usr/local/bin/fishlogin | sudo tee -a /etc/shells
 sudo usermod -s /usr/local/bin/fishlogin $USER
+
 echo Installed fish as the default shell with .profile inhertance from bash
