7a89ca82020e6a23207f36b5195e290fd05a8f4c
Marvin Löbel <loebel.marvin@gmail.com>
2025-06-14 17:53:07 +0200

    Make bootstrap script more robust

--- .dotfiles/system_setup/install_dotfiles_commands.sh
+++ .dotfiles/system_setup/install_dotfiles_commands.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-mkdir -p $HOME/.dotfiles
-git init --bare $HOME/.dotfiles/git_dir
-dotfiles="git --git-dir=$HOME/.dotfiles/git_dir --work-tree=$HOME"
-$dotfiles remote add origin git@github.com:Kimundi/dotfiles.git
-$dotfiles fetch
-$dotfiles reset origin/master
-$dotfiles restore $HOME/bin/dotfiles
+function setup {
+    mkdir -p $HOME/.dotfiles
+    git init --bare $HOME/.dotfiles/git_dir
+    dotfiles="git --git-dir=$HOME/.dotfiles/git_dir --work-tree=$HOME"
+    $dotfiles remote add origin git@github.com:Kimundi/dotfiles.git
+    $dotfiles fetch
+    $dotfiles reset origin/master
+    $dotfiles restore $HOME/bin/dotfiles
 
-echo "Manage your dotfiles via $HOME/bin/dotfiles"
+    echo "Done"
+    echo "Manage your dotfiles via $HOME/bin/dotfiles"
+}
+
+setup
